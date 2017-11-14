8f54273fb54491e9e67012b7ef33a3a7064ccf2d
Michael <Michael-Stuehler@web.de>
2017-11-14 16:31:20 +0100

    HTML seite, Hintergrundbild und main.py angepasst

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from flask import Flask
+import codecs
+#import tensorflow as tf
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    indexPage = open('HTML\index.html', 'r', encoding='utf-8')
+    return indexPage.read()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(debug=True)
+    #app.run(host='0.0.0.0')
